e302580123e1f210c060f0f3afa0d50bb1f59a39
TAIJULAMAN <taijulaman8@gmail.com>
2025-10-28 12:12:54 +0600

    add safe area in home page.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,25 @@
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import { useTheme } from "@/hooks/useTheme";
 import { Link } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
   const homeStyles = createHomeStyles(colors);
 
   return (
-    <View
-      style={homeStyles.container}
+    <SafeAreaView
+      style={homeStyles.safeArea}
     >
       <TouchableOpacity
         onPress={toggleDarkMode}
       >
         <Text>Toggle Theme</Text>
       </TouchableOpacity>
-
       <Link href="/about">About</Link>
-
-
-    </View>
+    </SafeAreaView>
   );
 }
 
